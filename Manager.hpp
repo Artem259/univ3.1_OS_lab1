8cda1a1f785a4f8d45cae6e1a7ff14fe6f1caf8b
Artem Poliakov <artem4250@gmail.com>
2022-10-21 14:36:20 +0300

    Manager class update

--- Manager.hpp
+++ Manager.hpp
@@ -17,6 +17,7 @@ private:
     std::variant<HARD_FAIL, SOFT_FAIL, int> resF, resG;
     int res;
     std::atomic_bool isTerminated;
+    int attemptsLeftF, attemptsLeftG;
 
 
     void xInputRequest() {
@@ -24,88 +25,109 @@ private:
         std::cin >> x;
     }
 
-    void createNamedPipes() {
-        pipeF = CreateNamedPipe(
-                R"(\\.\pipe\pipe_f)", // name of the pipe
-                PIPE_ACCESS_DUPLEX, // 2-way pipe
-                PIPE_TYPE_BYTE, // send data as a byte stream
-                1, // only allow 1 instance of this pipe
-                0, // no outbound buffer
-                0, // no inbound buffer
-                0, // use default wait time
-                nullptr // use default security attributes
-        );
-        if (pipeF == nullptr || pipeF == INVALID_HANDLE_VALUE) {
-            std::cerr << "[M] Failed to create pipeF instance.\n";
+    void createNamedPipe(char func) {
+        if (func != 'f' && func != 'g') {
+            std::cerr << "[M] Unexpected computation function: " << func << ".\n";
             throw std::runtime_error("");
         }
 
-        pipeG = CreateNamedPipe(
-                R"(\\.\pipe\pipe_g)", // name of the pipe
-                PIPE_ACCESS_DUPLEX, // 2-way pipe
-                PIPE_TYPE_BYTE, // send data as a byte stream
-                1, // only allow 1 instance of this pipe
-                0, // no outbound buffer
-                0, // no inbound buffer
-                0, // use default wait time
-                nullptr // use default security attributes
-        );
-        if (pipeG == nullptr || pipeG == INVALID_HANDLE_VALUE) {
-            std::cerr << "[M] Failed to create pipeG instance.\n";
-            throw std::runtime_error("");
+        if (func == 'f') {
+            pipeF = CreateNamedPipe(
+                    R"(\\.\pipe\pipe_f)", // name of the pipe
+                    PIPE_ACCESS_DUPLEX, // 2-way pipe
+                    PIPE_TYPE_BYTE, // send data as a byte stream
+                    1, // only allow 1 instance of this pipe
+                    0, // no outbound buffer
+                    0, // no inbound buffer
+                    0, // use default wait time
+                    nullptr // use default security attributes
+            );
+            if (pipeF == nullptr || pipeF == INVALID_HANDLE_VALUE) {
+                std::cerr << "[M] Failed to create pipeF instance.\n";
+                throw std::runtime_error("");
+            }
+        }
+        else {
+            pipeG = CreateNamedPipe(
+                    R"(\\.\pipe\pipe_g)", // name of the pipe
+                    PIPE_ACCESS_DUPLEX, // 2-way pipe
+                    PIPE_TYPE_BYTE, // send data as a byte stream
+                    1, // only allow 1 instance of this pipe
+                    0, // no outbound buffer
+                    0, // no inbound buffer
+                    0, // use default wait time
+                    nullptr // use default security attributes
+            );
+            if (pipeG == nullptr || pipeG == INVALID_HANDLE_VALUE) {
+                std::cerr << "[M] Failed to create pipeG instance.\n";
+                throw std::runtime_error("");
+            }
         }
     }
 
-    void createProcesses() {
-        STARTUPINFO siF, siG;
-
-        ZeroMemory(&siF, sizeof(siF));
-        siF.cb = sizeof(siF);
-        ZeroMemory(&piF, sizeof(piF));
-        ZeroMemory(&siG, sizeof(siG));
-        siG.cb = sizeof(siG);
-        ZeroMemory(&piG, sizeof(piG));
-
-        std::string cmdF = arg + " f " + std::to_string(x);
-        char* c_cmdF = const_cast<char*>(cmdF.c_str());
-        std::string cmdG = arg + " g " + std::to_string(x);
-        char* c_cmdG = const_cast<char*>(cmdG.c_str());
-
-        if (!CreateProcess( nullptr, // No module name (use command line)
-                            c_cmdF, // Command line
-                            nullptr, // Process handle not inheritable
-                            nullptr, // Thread handle not inheritable
-                            false, // Set handle inheritance to FALSE
-                            0, // Flags //CREATE_NEW_CONSOLE
-                            nullptr, // Use parent's environment block
-                            nullptr, // Use parent's starting directory
-                            &siF,  // Pointer to STARTUPINFO structure
-                            &piF ) // Pointer to PROCESS_INFORMATION structure
-                )
-        {
-            std::cerr << "[M] Failed to create process for f-function computation.\n";
+    void createProcess(char func) {
+        if (func != 'f' && func != 'g') {
+            std::cerr << "[M] Unexpected computation function: " << func << ".\n";
             throw std::runtime_error("");
         }
-        if (!CreateProcess( nullptr, // No module name (use command line)
-                            c_cmdG, // Command line
-                            nullptr, // Process handle not inheritable
-                            nullptr, // Thread handle not inheritable
-                            false, // Set handle inheritance to FALSE
-                            0, // Flags //CREATE_NEW_CONSOLE
-                            nullptr, // Use parent's environment block
-                            nullptr, // Use parent's starting directory
-                            &siG, // Pointer to STARTUPINFO structure
-                            &piG ) // Pointer to PROCESS_INFORMATION structure
-                )
-        {
-            std::cerr << "[M] Failed to create process for g-function computation.\n";
-            throw std::runtime_error("");
+
+        if (func == 'f') {
+            STARTUPINFO siF;
+
+            ZeroMemory(&siF, sizeof(siF));
+            siF.cb = sizeof(siF);
+            ZeroMemory(&piF, sizeof(piF));
+
+            std::string cmdF = arg + " f " + std::to_string(x);
+            char* c_cmdF = const_cast<char*>(cmdF.c_str());
+
+            if (!CreateProcess( nullptr, // No module name (use command line)
+                                c_cmdF, // Command line
+                                nullptr, // Process handle not inheritable
+                                nullptr, // Thread handle not inheritable
+                                false, // Set handle inheritance to FALSE
+                                0, // Flags //CREATE_NEW_CONSOLE
+                                nullptr, // Use parent's environment block
+                                nullptr, // Use parent's starting directory
+                                &siF,  // Pointer to STARTUPINFO structure
+                                &piF ) // Pointer to PROCESS_INFORMATION structure
+                    )
+            {
+                std::cerr << "[M] Failed to create process for f-function computation.\n";
+                throw std::runtime_error("");
+            }
+        }
+        else {
+            STARTUPINFO siG;
+
+            ZeroMemory(&siG, sizeof(siG));
+            siG.cb = sizeof(siG);
+            ZeroMemory(&piG, sizeof(piG));
+
+            std::string cmdG = arg + " g " + std::to_string(x);
+            char* c_cmdG = const_cast<char*>(cmdG.c_str());
+
+            if (!CreateProcess( nullptr, // No module name (use command line)
+                                c_cmdG, // Command line
+                                nullptr, // Process handle not inheritable
+                                nullptr, // Thread handle not inheritable
+                                false, // Set handle inheritance to FALSE
+                                0, // Flags //CREATE_NEW_CONSOLE
+                                nullptr, // Use parent's environment block
+                                nullptr, // Use parent's starting directory
+                                &siG, // Pointer to STARTUPINFO structure
+                                &piG ) // Pointer to PROCESS_INFORMATION structure
+                    )
+            {
+                std::cerr << "[M] Failed to create process for g-function computation.\n";
+                throw std::runtime_error("");
+            }
         }
     }
 
     static std::variant<HARD_FAIL, SOFT_FAIL, int> readResultFromNamedPipe(char func, Manager* manager, HANDLE* pipe, std::mutex* mx) {
         ConnectNamedPipe(*pipe, nullptr);
-        if (manager->isComputationTerminated()) {
+        if (manager->isTerminated) {
             return {};
         }
 
@@ -123,14 +145,35 @@ private:
             throw std::runtime_error("");
         }
         auto* funcRes = static_cast<std::variant<HARD_FAIL, SOFT_FAIL, int>*>(buffer);
+        bool goAttempt = false;
         mx->lock();
         if (!manager->isTerminated) {
-            std::cout << " -- " << func << "(x): " << *funcRes << "\n";
-            if (std::holds_alternative<HARD_FAIL>(*funcRes) || std::holds_alternative<SOFT_FAIL>(*funcRes)) { // TODO: SOFT_FAIL
+            if (std::holds_alternative<HARD_FAIL>(*funcRes)) {
+                std::cout << " -- " << func << "(x): " << *funcRes << "\n";
                 manager->terminateProcessAndPipe(func == 'f' ? 'g' : 'f');
+            }
+            else if (std::holds_alternative<SOFT_FAIL>(*funcRes)) {
+                manager->decrementAttemptsLeft(func);
+                int attemptsLeft = manager->attemptsLeft(func);
+                if (attemptsLeft > 0) {
+                    std::cout << " -- " << func << "(x): " << *funcRes << " [" << attemptsLeft << "]\n";
+                    manager->closeHandles(func);
+                    manager->createNamedPipe(func);
+                    manager->createProcess(func);
+                    goAttempt = true;
+                }
+                else {
+                    std::cout << " -- " << func << "(x): " << *funcRes << "\n";
+                    manager->terminateProcessAndPipe(func == 'f' ? 'g' : 'f');
+                }
+            }
+            else {
+                std::cout << " -- " << func << "(x): " << *funcRes << "\n";
             }
         }
         mx->unlock();
+        if (goAttempt)
+            return readResultFromNamedPipe(func, manager, pipe, mx);
         return *funcRes;
     }
 
@@ -161,21 +204,21 @@ private:
         }
     }
 
-    void terminateProcessAndPipe(char forFunc) {
-        if (forFunc != 'f' && forFunc != 'g') {
+    void terminateProcessAndPipe(char func) {
+        if (func != 'f' && func != 'g') {
             std::cerr << "[M] Unexpected terminateProcessAndPipe() function parameter.\n";
             throw std::runtime_error("");
         }
 
         isTerminated = true;
-        if (forFunc == 'f') {
+        if (func == 'f') {
             TerminateProcess(piF.hProcess, 1);
         }
         else { // forFunc == 'g'
             TerminateProcess(piG.hProcess, 1);
         }
 
-        std::string pipeName = std::string(R"(\\.\pipe\pipe_)") + forFunc;
+        std::string pipeName = std::string(R"(\\.\pipe\pipe_)") + func;
         char* c_pipeName = const_cast<char*>(pipeName.c_str());
 
         CreateFile(
@@ -189,18 +232,51 @@ private:
         );
     }
 
-    void closeHandles() {
-        CloseHandle(pipeF);
-        CloseHandle(piF.hProcess);
-        CloseHandle(piF.hThread);
+    void closeHandles(char func) {
+        if (func != 'f' && func != 'g') {
+            std::cerr << "[M] Unexpected computation function: " << func << ".\n";
+            throw std::runtime_error("");
+        }
 
-        CloseHandle(pipeG);
-        CloseHandle(piG.hProcess);
-        CloseHandle(piG.hThread);
+        if (func == 'f') {
+            DisconnectNamedPipe(pipeF);
+            CloseHandle(pipeF);
+            CloseHandle(piF.hProcess);
+            CloseHandle(piF.hThread);
+        }
+        else {
+            DisconnectNamedPipe(pipeG);
+            CloseHandle(pipeG);
+            CloseHandle(piG.hProcess);
+            CloseHandle(piG.hThread);
+        }
+    }
+
+    int attemptsLeft(char func) const {
+        if (func != 'f' && func != 'g') {
+            std::cerr << "[M] Unexpected computation function: " << func << ".\n";
+            throw std::runtime_error("");
+        }
+
+        if (func == 'f')
+            return attemptsLeftF;
+        return attemptsLeftG;
+    }
+
+    void decrementAttemptsLeft(char func) {
+        if (func != 'f' && func != 'g') {
+            std::cerr << "[M] Unexpected computation function: " << func << ".\n";
+            throw std::runtime_error("");
+        }
+
+        if (func == 'f')
+            attemptsLeftF--;
+        else
+            attemptsLeftG--;
     }
 
 public:
-    explicit Manager(std::string arg) : arg(std::move(arg)) {
+    explicit Manager(std::string arg, int maxAmountOfAttempts) : arg(std::move(arg)) {
         x = 0;
         piF = {};
         piG = {};
@@ -208,20 +284,20 @@ public:
         pipeG = nullptr;
         res = -1;
         isTerminated = false;
-    }
-
-
-    std::atomic_bool& isComputationTerminated() {
-        return isTerminated;
+        attemptsLeftF = maxAmountOfAttempts;
+        attemptsLeftG = maxAmountOfAttempts;
     }
 
     void run() {
         xInputRequest();
-        createNamedPipes();
-        createProcesses();
+        createNamedPipe('f');
+        createNamedPipe('g');
+        createProcess('f');
+        createProcess('g');
         receiveResults(); // resF, resG
         calculateFinalResult();
-        closeHandles();
+        closeHandles('f');
+        closeHandles('g');
     }
 };
 
